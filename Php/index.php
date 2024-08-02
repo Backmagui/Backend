e125099a803675082b1e06ff67e0461701f8891d
Backmagui <guilherme.mata.senai404@gmail.com>
2024-08-02 10:34:44 -0300

    Add files via upload

---
+++ Php/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome = 'Guilherme';
+$escola = 'Sesi';
+$curso = 'Desenvolvimento de Sistemas';
+$idade = 16 ;
+$rm = 3917;
+$dinheiro = 167.48;
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$curso<br>";
+echo "$idade<br>";
+echo "$rm<br>";
+echo "$dinheiro<br>";
+
+echo "Olá, meu nome é $nome, tenho $idade, estudo no $escola, 
+cursando $curso, obtendo R$ $dinheiro na minha conta";
+
+echo 'Hello world<br>';
+//Este é um comentário, bem legal!
+
+echo 'Senai, estamos aprendendo';
+
+/*O código "echo" serve para você escrever algo no seu site, 
+funcionando como um "p" no html, porém é preciso 
+utilizar o ";" ao final da linha do seu código
+
+O "<br>" é utilizado para pular a linha, tendo que ser usado dentro da '' do texto */
+?>
