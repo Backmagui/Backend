fdf5de983c0a17d29219fb58b12b38a41a747171
Backmagui <guilherme.mata.senai404@gmail.com>
2024-08-23 09:08:40 -0300

    Add files via upload

---
+++ Php/exercicio8.php
@@ -0,0 +1,15 @@
+<?php
+
+$soma= 0;
+
+//Condição for para fazer a conta
+for ($numero = 0; $numero <=100; $numero++){
+    //Somando os números
+    $soma = $numero + $soma;
+    //Imprimindo
+    echo "$soma<br>";
+}
+
+
+
+?>
