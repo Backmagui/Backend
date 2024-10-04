707689187cbc0cc8b115a9fc6ce50ec496a0ce5a
Backmagui <guilherme.mata.senai404@gmail.com>
2024-10-04 10:51:41 -0300

    Add files via upload

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -33,6 +33,27 @@
                 <div class="form-group">
                   <input type="password" name="senha" class="form-control" placeholder="Senha" required>
                 </div>
+
+                <?php
+                if (isset($_GET['login']) && $_GET['login'] == 'erro'){
+                ?>
+                <div class="text-danger">
+                  Usuário e/ou senha sem cadastro
+                </div>
+                <?php
+                }
+                ?>
+
+                <?php
+                if (isset($_GET['login']) && $_GET['login'] == 'erro2'){
+                ?>
+                <div class="text-danger">
+                  Faça o login primeiro
+                </div>
+                <?php
+                }
+                ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- app_help_desk/painel.php
+++ app_help_desk/painel.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+//echo $_SESSION['autenticado'];
+
+if (!isset($_SESSION['autenticado']) OR $_SESSION['autenticado'] !='SIM'){
+  header ('Loction: index.php?login=erro2');
+}
+
+?>
+
+
 <html>
   <head>
     <meta charset="utf-8" />
